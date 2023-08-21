332e5dfbfcd8a370c23a258b6d62f1ae91c1fc76
Beatriz <beatrizleite_aragao@hotmail.com>
2023-08-21 02:00:52 -0300

    template configuration and html files

--- blog/models/post.py
+++ blog/models/post.py
@@ -8,7 +8,7 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200 , unique=True)
+    slug = models.SlugField(max_length=200 , unique=True)#endereços unicos para objetos
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     update_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
